bf95c4d76a767cc2fbf5aa34f581d32dbeab5dee
Sunny Kaushik <sunny.kaushik2305@gmail.com>
2024-08-21 09:08:09 +0530

    Add files via upload

---
+++ cal.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# A simple calculator shell program
+
+a=$1
+b=$2
+
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+ch=$3
+
+case $ch in
+    1) res=`expr $a + $b`
+    ;;
+    2) res=`expr $a - $b`
+    ;;
+    3) res=`expr $a \* $b`
+    ;;
+    4) res=`expr $a / $b`
+    ;;
+    *) echo "Invalid choice"; exit 1
+    ;;
+esac
+
+echo "Result : $res"
